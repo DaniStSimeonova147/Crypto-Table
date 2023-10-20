b6f9dc8e0ad20b2c8de6a2304294c5387ff35bb7
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-20 18:23:18 +0300

    Add edit

--- src/controllers/cryptoController.js
+++ src/controllers/cryptoController.js
@@ -8,7 +8,7 @@ const { isAuth } = require('../middlewares/authMiddleware');
 router.get('/cataloge', async (req, res) => {
     const cryptos = await cryptoManager.getAll().lean();
 
-    res.render('cryptos/cataloge',{cryptos});
+    res.render('cryptos/cataloge', { cryptos });
 });
 
 router.get('/create', async (req, res) => {
@@ -21,13 +21,13 @@ router.post('/create', isAuth, async (req, res) => {
         ...req.body,
         owner: req.user._id,
     };
-    
+
     try {
         await cryptoManager.create(cryptoData);
 
         res.redirect('/cryptos/cataloge');
     } catch (error) {
-       res.render('cryptos/create', { error: getErrorMessage(error) });
+        res.render('cryptos/create', { error: getErrorMessage(error) });
     }
 });
 
@@ -39,6 +39,36 @@ router.get('/:cryptoId/details', async (req, res) => {
 
     res.render('cryptos/details', { crypto, isOwner });
 });
+
+router.get('/:cryptoId/delete', isAuth, async (req, res) => {
+    const cryptoId = req.params.cryptoId;
+    try {
+        await cryptoManager.delete(cryptoId);
+
+        res.redirect('/cryptos/cataloge');
+    } catch (err) {
+        res.render(`/cryptos/${cryptoId}/details`, { error: 'Unsuccessful photo delete!' });
+    }
+});
+
+router.get('/:cryptoId/edit', isAuth, async (req, res) => {
+    const crypto = await cryptoManager.getOne(req.params.cryptoId).lean();
+    res.render('cryptos/edit', { crypto });
+});
+
+router.post('/:cryptoId/edit', isAuth, async (req, res) => {
+    const cryptoId = req.params.cryptoId;
+    const cryptoData = req.body;
+
+    try {
+        await cryptoManager.edit(cryptoId, cryptoData);
+
+        res.redirect(`/cryptos/${cryptoId}/details`);
+    } catch (error) {
+        res.render('cryptos/edit', { error: 'Unable to update crypto!', ...cryptoData });
+    }
+});
+
 
 
 
--- src/managers/cryptoManager.js
+++ src/managers/cryptoManager.js
@@ -5,3 +5,7 @@ exports.getAll = () => Crypto.find().populate('owner');
 exports.getOne = (cryptoId) => Crypto.findById(cryptoId).populate('owner');
 
 exports.create = (cryptoData) => Crypto.create(cryptoData);
+
+exports.delete = (cryptoId) => Crypto.findByIdAndDelete(cryptoId);
+
+exports.edit = (cryptoId, cryptoData) => Crypto.findByIdAndUpdate(cryptoId, cryptoData);
